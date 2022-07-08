bb92c1b0e93f90747e9a42610b84638b699f3d92
Elena <9513495670@mail.ru>
2022-07-08 21:54:02 +0300

    added task_2

---
+++ task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа на вход принимает два числа и проверяет, является ли
+// первое число квадратом второго.
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int squ=num2*num2;
+if (num1==squ)
+{
+    Console.WriteLine($"{num1} квадрат числа {num2}");
+}
+else
+{
+    Console.WriteLine($"{num1} не является квадратом числа {num2}");
+}
